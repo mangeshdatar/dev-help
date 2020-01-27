3a72267c11141b8cdcca5900bb75c39be6d029a3
Nachiket Salvi <nsalvi@mobiquityinc.com>
2020-01-27 14:40:04 +0530

    Added initial zoo configurtion

---
+++ scripts/create-config.sh
@@ -0,0 +1,5 @@
+# If you need to add another file built from configuration, you may add it
+# here. `npx envsub` will replace all `${VAR_NAME}` with a corresponding
+# environment variable.
+
+npx envsub src/environments/build.config.ts src/environments/environment.ts
