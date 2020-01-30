4da3c4a492e54d0140e8d278efe9cf927f7762bd
Mangesh55 <mdatar@mobiquityinc.com>
2020-01-30 18:03:13 +0530

    Added Hide/Show Header functionality.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  content;
+  show = false;
   constructor() { }
 
   ngOnInit() {
+    this.content = document.getElementById('content');
+    console.log(this.content);
+    this.content.addEventListener('wheel', this.called);
   }
+  called(event) {
+    var delta;
+        	if (event.wheelDelta){
+            	delta = event.wheelDelta;
+            }else{
+            	delta = -1 *event.deltaY;
+          }
+    setTimeout(() => {
+      const header: any = document.getElementsByClassName('search-bar-container');
 
+      if (delta < 0){
+        console.log("DOWN");
+        this.show = false;
+        header[0].style.display = "none";
+
+        return
+        }else if (delta > 0){
+        console.log("UP");
+        header[0].style.display = "block";
+
+        console.log(header[0]);
+        this.show = true
+
+        }
+    },500)
+       
+}
 }
